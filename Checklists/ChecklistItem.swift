9d509c57efd30cd7917f31111a0ecaf11b8fff36
Julia <julia.gurbanova@gmail.com>
2023-12-24 20:40:14 +0300

    initial commit

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,13 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Julia Gurbanova on 17.12.2023.
+//
+
+import Foundation
+
+class ChecklistItem: NSObject, Codable {
+    var text = ""
+    var checked = false
+}
