0feb95956f5dc7c7394bd7adfbfd361ea43bdc7e
Julia <julia.gurbanova@gmail.com>
2023-12-26 21:42:33 +0300

    show, add, edit multiple checklists

---
+++ Checklists/Checklist.swift
@@ -0,0 +1,17 @@
+//
+//  Checklist.swift
+//  Checklists
+//
+//  Created by Julia Gurbanova on 25.12.2023.
+//
+
+import UIKit
+
+class Checklist: NSObject {
+    var name = ""
+
+    init(name: String) {
+        self.name = name
+        super.init()
+    }
+}
--- Checklists/CheckllistViewController.swift
+++ Checklists/CheckllistViewController.swift
@@ -10,13 +10,16 @@ import UIKit
 class ChecklistViewController: UITableViewController, ItemDetailViewControllerDelegate {
 
     var items = [ChecklistItem]()
+    var checklist: Checklist!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.largeTitleDisplayMode = .never
 
         loadChecklistItems()
+
+        title = checklist.name
 
     }
 
